fa0ee88b501ce6e7bef63ccdeb5f07e96c6b2f44
enzofreitas98 <128050050+enzofreitas98@users.noreply.github.com>
2023-06-10 20:58:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,16 +14,16 @@ logging.basicConfig(level=logging.INFO)
 url = 'https://double.turbogames.io/'
 
 # Configure Chrome driver options
-chrome_driver_options = webdriver.ChromeOptions()
-chrome_driver_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_driver_options.add_argument("--headless")
-chrome_driver_options.add_argument("--no-sandbox")
-chrome_driver_options.add_argument("--disable-dev-shm-usage")
-chrome_driver_options.add_argument("--remote-debugging-port=9222")
-chrome_driver_options.add_argument("--single-process")  # Adicione esta linha
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+
+
 
 # Initialize the WebDriver
-driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), chrome_options=chrome_options)
+driver=webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
 driver.get(url)
 
 # Initialize global variable
