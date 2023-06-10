87a7a3c02ce136164c40962ac27a4c851193edcd
enzofreitas98 <128050050+enzofreitas98@users.noreply.github.com>
2023-06-10 20:42:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import os
 import logging
-from selenium.webdriver.chrome.service import Service
 from threading import Thread
 import time
 
@@ -13,17 +12,17 @@ logging.basicConfig(level=logging.INFO)
 
 url = 'https://double.turbogames.io/'
 
-# Configure Chrome options
-chrome_options = Options()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--remote-debugging-port=9222")
+# Configure Chrome driver options
+chrome_driver_options = webdriver.ChromeOptions()
+chrome_driver_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_driver_options.add_argument("--headless")
+chrome_driver_options.add_argument("--no-sandbox")
+chrome_driver_options.add_argument("--disable-dev-shm-usage")
+chrome_driver_options.add_argument("--remote-debugging-port=9222")
+chrome_driver_options.add_argument("--single-process")  # Adicione esta linha
 
 # Initialize the WebDriver
-driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_driver_options)
 driver.get(url)
 
 # Initialize global variable
