f925de87c792250c335a92dee9e7cddd21280b87
enzofreitas98 <128050050+enzofreitas98@users.noreply.github.com>
2023-06-10 20:26:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import os
 import logging
 from selenium.webdriver.chrome.service import Service
 from threading import Thread
-import time
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -22,11 +21,9 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--remote-debugging-port=9222")
 
-
-    # Initialize the WebDriver
+# Initialize the WebDriver
 driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
 driver.get(url)
-
 
 # Initialize global variable
 numeric_value = ""
@@ -34,22 +31,23 @@ numeric_value = ""
 def update_numeric_value():
     global numeric_value
     while True:
-            # Obter o conteúdo completo da página
-            page_source = driver.page_source
+        # Obter o conteúdo completo da página
+        page_source = driver.page_source
 
-            # Criar um objeto BeautifulSoup para analisar o conteúdo da página
-            soup = BeautifulSoup(page_source, 'html.parser')
+        # Criar um objeto BeautifulSoup para analisar o conteúdo da página
+        soup = BeautifulSoup(page_source, 'html.parser')
 
-            # Extrair o conteúdo específico usando métodos do BeautifulSoup
-            # Por exemplo, suponha que você queira extrair o conteúdo de uma div com a classe "my-div-class"
-            div_content = soup.find('div', class_='active').text
-            numeric_value = div_content
+        # Extrair o conteúdo específico usando métodos do BeautifulSoup
+        # Por exemplo, suponha que você queira extrair o conteúdo de uma div com a classe "my-div-class"
+        div_content = soup.find('div', class_='active').text
+        numeric_value = div_content
 
 @app.route('/get_numeric_value', methods=['GET'])
-def capture_and_transcribe():
+def get_numeric_value():
+    global numeric_value
     return jsonify({'numeric_value': numeric_value})
 
-if __name__=='__main__':
+if __name__ == '__main__':
     # Start the background task
     t = Thread(target=update_numeric_value)
     t.start()
