94c18a199e7c51b06caf70a47efeaa9409e515dd
enzofreitas98 <128050050+enzofreitas98@users.noreply.github.com>
2023-06-10 22:27:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
 
-
+#
 # Initialize the WebDriver
 driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
 driver.get(url)
