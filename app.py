e747cd4629afbdb70826f395a7656d4d7e34eb92
enzofreitas98 <128050050+enzofreitas98@users.noreply.github.com>
2023-06-11 00:22:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import undetected_chromedriver as uc
-driver = uc.Chrome()
+from selenium import webdriver
+import os
+
+options = webdriver.ChromeOptions() 
+options.add_argument("start-maximized")
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = uc.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
 driver.get("https://double.turbogames.io/")
 print(driver.page_source)
