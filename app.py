72130972e20149336032857418ca955ffb6be8f8
enzofreitas98 <128050050+enzofreitas98@users.noreply.github.com>
2023-06-10 21:10:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -26,32 +26,11 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
 driver.get(url)
 
-# Initialize global variable
-numeric_value = ""
-
-def update_numeric_value():
-    global numeric_value
-    while True:
-        # Obter o conteúdo completo da página
-        page_source = driver.page_source
-
-        # Criar um objeto BeautifulSoup para analisar o conteúdo da página
-        soup = BeautifulSoup(page_source, 'html.parser')
-
-        # Extrair o conteúdo específico usando métodos do BeautifulSoup
-        # Por exemplo, suponha que você queira extrair o conteúdo de uma div com a classe "my-div-class"
-        div_content = soup.find('div', class_='active').text
-        numeric_value = div_content
-
-@app.route('/get_numeric_value', methods=['GET'])
-def get_numeric_value():
-    global numeric_value
-    return jsonify({'numeric_value': numeric_value})
+print(driver.page_source)
 
 if __name__ == '__main__':
     # Start the background task
-    t = Thread(target=update_numeric_value)
-    t.start()
+
 
     # Start the Flask app
     app.run(port=5000, debug=True)
